d840baffc577914e721ebc6064b784e33f78c683
田昊东 <102659095+thdlrt@users.noreply.github.com>
2024-04-07 17:26:21 +0800

    s4

--- tinyraytracer.cpp
+++ tinyraytracer.cpp
@@ -11,9 +11,19 @@ const int height   = 768;
 const float fov    = M_PI/2;
 Material      ivory(Vec3f(0.4, 0.4, 0.3));
 Material red_rubber(Vec3f(0.3, 0.1, 0.1));
-Material backgroud(Vec3f(0.2, 0.4, 0.6));
+Material backgroud(Vec3f(0.2, 0.7, 0.8));
+Material black(Vec3f(0, 0, 0));
+std::vector<Sphere> spheres = {
+    Sphere(Vec3f(-3, 0, -16), 2, ivory),
+    Sphere(Vec3f(-1.0, -1.5, -12), 2, red_rubber),
+    Sphere(Vec3f(1.5, -0.5, -18), 3, red_rubber),
+    Sphere(Vec3f(7, 5, -18), 4, ivory)
+};
+std::vector<PointLight> lights = {
+    PointLight(Vec3f(-20, 20, 20), 1.5)
+};
 
-bool intersect(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres, Vec3f &hit_p, Vec3f &normal, Material &material) {
+bool intersect(const Vec3f &orig, const Vec3f &dir, Vec3f &hit_p, Vec3f &normal, Material &material) {
     float spheres_dist = std::numeric_limits<float>::max();
     for (size_t i=0; i<spheres.size(); i++) {
         float dist_i;
@@ -27,22 +37,26 @@ bool intersect(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &s
     }
     return spheres_dist<1000;
 }
-Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres) {
+Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir) {
     Vec3f hit_p, normal;
     Material material;
-    if (!intersect(orig, dir, spheres, hit_p, normal, material)) {
+    if (!intersect(orig, dir, hit_p, normal, material)) {
         return backgroud.color;
     }
-    return material.color;
+    float diffuse_intensity = 0;
+    for(PointLight light: lights) {
+        Vec3f light_dir = (light.position - hit_p).normalize();
+        diffuse_intensity += light.intensity * std::max(0.f, light_dir*normal);
+    }
+    return material.color*diffuse_intensity;
 }
 void renderpix(std::vector<Vec3f>&framebuffer) {
-    std::vector<Sphere>spheres={Sphere(Vec3f(1, 1, -10), 1, ivory), Sphere(Vec3f(5, 5, -10), 2, red_rubber)};
     for (int j = 0; j<height; j++) {
         for (int i = 0; i<width; i++) {
             float x =  (2*(i + 0.5)/(float)width  - 1)*tan(fov/2.)*width/(float)height;
             float y = -(2*(j + 0.5)/(float)height - 1)*tan(fov/2.);
             Vec3f dir = Vec3f(x, y, -1).normalize();
-            framebuffer[i+j*width] = cast_ray(Vec3f(0,0,0), dir, spheres);
+            framebuffer[i+j*width] = cast_ray(Vec3f(0,0,0), dir);
         }
     }
 }
@@ -50,11 +64,11 @@ void renderpix(std::vector<Vec3f>&framebuffer) {
 void render() {
     std::vector<Vec3f> framebuffer(width*height);
     renderpix(framebuffer);
-    std::ofstream ofs; // save the framebuffer to file
-    ofs.open("./out.ppm");
+    std::ofstream ofs;
+    ofs.open("./out.ppm", std::ofstream::out | std::ofstream::binary);
     ofs << "P6\n" << width << " " << height << "\n255\n";
-    for (size_t i = 0; i < height*width; ++i) {
-        for (size_t j = 0; j<3; j++) {
+    for (int i = 0; i < height*width; ++i) {
+        for (int j = 0; j<3; j++) {
             ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
         }
     }
